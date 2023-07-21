376b7b107df4f0eb2ff36403a474feac84dc1827
Love-way <82093150+Love-way@users.noreply.github.com>
2023-07-21 10:51:16 +0000

    Add files via upload

---
+++ Flood_Detection.ino
@@ -0,0 +1,31 @@
+int resval = 0;  // holds the value
+int respin = A5; // sensor pin used
+  
+void setup() { 
+ 
+  // start the serial console
+  Serial.begin(9600);
+} 
+  
+void loop() { 
+   
+  resval = analogRead(respin); //Read data from analog pin and store it to resval variable
+   
+  if (resval<=100)
+    { 
+     Serial.println("Water Level: Good water level, Not to worry"); 
+    } 
+     else if (resval>100 && resval<=300)
+    { 
+    Serial.println("Water Level: Too much water in room"); 
+    } 
+    else if (resval>300 && resval<=330)
+    { 
+    Serial.println("Water Level: Medium"); 
+    }
+    else if (resval>330)
+    { 
+    Serial.println("Water Level: High"); 
+    }
+  delay(1000); 
+}
